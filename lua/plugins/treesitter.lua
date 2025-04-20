73648704e34965a47e814510729480ae133dfb35
eomhs <jesikajunior@hanmail.net>
2025-04-20 14:13:17 +0900

    Add initial configuration files for Neovim setup including lazy loading and plugins

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua", "vim", "vimdoc", "bash", "json", "html", "javascript", "c", "go"
+      },
+      highlight = {
+        enable = true,      
+	additional_vim_regex_highlighting = false,			
+      },
+      indent = {
+        enable = true,     
+      },
+    })
+  end
+}
+
